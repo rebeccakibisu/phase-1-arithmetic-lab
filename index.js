b7c33d730bfa6fa303fd91452599a14f1063f13f
Rebecca Kibisu <coursecpd@gmail.com>
2025-06-16 06:10:39 +0300

    arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * 2) - 3;
+    
+let num3 = 28;
+let num4 = 6;
+let mod = num3 % num4;
+
+let max = Math.max(10, 5, 20, 4, 15);
+
+
